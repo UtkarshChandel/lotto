9dce2862ffda70a0ed75523bc15e8e4576489cd4
UtkarshChandel <personal.utkarsh24@gmail.com>
2020-04-06 19:48:33 +0530

    Updates

---
+++ precache-manifest.3275fe44f22971c70123b504ff74e63a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fe305bc83ad77477c7458b46d38216",
+    "url": "/lotto/index.html"
+  },
+  {
+    "revision": "8ceae1ad716b45cc4ade",
+    "url": "/lotto/static/css/main.d613338c.chunk.css"
+  },
+  {
+    "revision": "7b1ac68b6ead89c83568",
+    "url": "/lotto/static/js/2.1440862d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lotto/static/js/2.1440862d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ceae1ad716b45cc4ade",
+    "url": "/lotto/static/js/main.3d04fcb1.chunk.js"
+  },
+  {
+    "revision": "d8c3617791935a8d9f17",
+    "url": "/lotto/static/js/runtime-main.9fe19336.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lotto/static/media/logo.5d5d9eef.svg"
+  }
+]);
